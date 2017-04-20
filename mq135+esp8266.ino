ddb61710bc0f04cb8e8b7214c30521b3a2638da2
francuzzz <france83ukg@mail.ru>
2017-04-20 13:34:02 +0600

    Update mq135+esp8266.ino

--- mq135+esp8266.ino
+++ mq135+esp8266.ino
@@ -1,1 +1,59 @@
-# MQ135-ESP8266-NArodmon-MQTT
+#include <DHT.h>
+#include <DHT_U.h>
+#include <MQ135.h>
+#define DHTTYPE DHT11 // Тип датчика
+#define DHTPIN 5 //К какому пину подключен датчик DHT
+DHT dht (DHTPIN, DHTTYPE);
+
+MQ135 gasSensor = MQ135 (A0); //Датчик газа подключен к аналоговому пину
+
+
+#include <TimeLib.h>
+#include <SimpleTimer.h>
+SimpleTimer timer;
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  timer.setInterval(5000L,sendTemps);
+
+}
+
+void sendTemps (){
+
+float h = dht.readHumidity(); 
+float t = dht.readTemperature(); 
+
+ if (isnan(h) || isnan(t)){
+     return;
+     }
+
+float rzero = gasSensor.getRZero();
+float ppm = gasSensor.getPPM();
+float rzeroc = gasSensor.getCorrectedRZero(t, h);
+float ppmc = gasSensor.getCorrectedPPM(t, h);
+
+
+Serial.print("Humidity: ");
+  Serial.println(h);
+  Serial.print("Temperature: ");
+  Serial.println(t);
+  Serial.print("A0: ");
+  Serial.println(analogRead (A0));
+  Serial.print("Rzero: ");
+  Serial.println(rzero);
+  Serial.print("Rzeroc: ");
+  Serial.println(rzeroc);
+  Serial.print("PPM: ");
+  Serial.println(ppm);
+  Serial.print("PPMC: ");
+  Serial.println(ppmc);
+  Serial.println();
+
+}
+void loop() {
+  timer.run();
+
+}
+
+
